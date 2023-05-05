d71da83496b9ed72c4e4bdb19fd91e5a5a2a44f8
TheX3r0 <X3R0_SEXY@outlook.com>
2023-05-04 21:45:04 -0500

    clase alimento

---
+++ model/Alimento.py
@@ -0,0 +1,38 @@
+class Alimento:
+    def __init__(self):
+        self.carnivoros = set()
+        self.herbivoros = set()
+    
+    def addCarnivoros(self, alimento):
+        self.carnivoros.add(alimento)
+    
+    def addHerbivoros(self, alimento):
+        self.herbivoros.add(alimento)
+    
+    def verificarCarnivoros(self, alimento):
+        return alimento in self.carnivoros
+    
+    def verificarHerbivoros(self, nombre):
+        return alimento in self.herbivoros
+
+    def imprimirAlimentos(self):
+        print("Carnivoros\tHerbivoros")
+        print("----------\t----------")
+        for i, j in zip(self.carnivoros, self.herbivoros):
+            print(f"{i}\t\t{j}")
+
+    def editAlimento(self, alimento_old, alimento_new, tipo_animal):
+        if tipo_animal == "carnivoros":
+            if alimento_old in self.carnivoros:
+                self.carnivoros.discard(alimento_old)
+                self.carnivoros.add(alimento_new)
+            else:
+                print("El alimento no se encuentra en la lista de alimentos para carnívoros.")
+        elif tipo_animal == "herbivoros":
+            if alimento_old in self.herbivoros:
+                self.herbivoros.discard(alimento_old)
+                self.herbivoros.add(alimento_new)
+            else:
+                print("El alimento no se encuentra en la lista de alimentos para herbívoros.")
+
+
