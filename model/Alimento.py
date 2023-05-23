8fe7c9e66df80125988352996fa6e8865d561943
TheX3r0 <X3R0_SEXY@outlook.com>
2023-05-23 00:08:11 -0500

    anulando borrado de funciones de alimentos

--- model/Alimento.py
+++ model/Alimento.py
@@ -35,4 +35,43 @@ class Alimento:
             self.semiAcuaticoH.append(alimento)
         elif tipoAnimal == 4:
             self.voladorH.append(alimento)
+     def verificarCarnivoros(self, alimento, tipoAnimal):
+        if tipoAnimal == 1:
+            return alimento in self.terrestresC
+        elif tipoAnimal == 2:
+            return alimento in self.acuaticoC
+        elif tipoAnimal == 3:
+            return alimento in self.semiAcuaticoC
+        elif tipoAnimal == 4:
+            return alimento in self.voladorC
+
+    def verificarHerbivoros(self, alimento, tipoAnimal):
+        if tipoAnimal == 1:
+            return alimento in self.terrestresH
+        elif tipoAnimal == 2:
+            return alimento in self.acuaticoH
+        elif tipoAnimal == 3:
+            return alimento in self.semiAcuaticoH
+        elif tipoAnimal == 4:
+            return alimento in self.voladorH
+
+    def deleteAlimentoC(self, alimento, tipoAnimal):
+        if tipoAnimal == 1:
+            self.terrestresC.remove(alimento)
+        elif tipoAnimal == 2:
+            self.acuaticoC.remove(alimento)
+        elif tipoAnimal == 3:
+            self.semiAcuaticoC.remove(alimento)
+        elif tipoAnimal == 4:
+            self.voladorC.remove(alimento)
+    
+    def deleteAlimentoH(self,alimento,tipoAnimal):
+        if tipoAnimal == 1:
+            self.terrestresH.remove(alimento)
+        elif tipoAnimal == 2:
+            self.acuaticoH.remove(alimento)
+        elif tipoAnimal == 3:
+            self.semiAcuaticoH.remove(alimento)
+        elif tipoAnimal == 4:
+            self.voladorH.remove(alimento)
 
